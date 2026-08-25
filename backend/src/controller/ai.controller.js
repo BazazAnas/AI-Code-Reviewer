6f129f14b79686e8ef096f2b58599654fae4c9c1
Anas Bin Ajaz <123629492+BazazAnas@users.noreply.github.com>
2026-08-25 23:35:46 +0530

    Update ai.controller.js

--- backend/src/controller/ai.controller.js
+++ backend/src/controller/ai.controller.js
@@ -2,12 +2,9 @@ import main from "../services/ai.services.js";
 
 export const getResponse = async (req, res) => {
     const { code } = req.body;
-
     if (!code) {
         return res.status(400).json({ message: "code is required" });
     }
-
     const result = await main(code);
-
     res.status(200).send(result)
 }
